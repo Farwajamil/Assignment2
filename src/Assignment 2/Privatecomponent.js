2070f7db58b7fa19f098d7c14378397d9f2e5e12
Farwajamil <102739780+Farwajamil@users.noreply.github.com>
2022-04-14 14:49:54 +0500

    Delete Privatecomponent.js

--- src/Assignment 2/Privatecomponent.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate ,Outlet } from "react-router-dom";
-function Privatecomponent()
-{
-    return(
-        const auth =
-    );
-}
-export default Privatecomponent;
